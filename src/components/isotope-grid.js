44d203e876eb7054a4ca00bb649482b731b77309
theremotecreative <jason@theremotecreative.com>
2022-02-22 11:06:51 -0600

    revisions

--- src/components/isotope-grid.js
+++ src/components/isotope-grid.js
@@ -83,6 +83,7 @@ const IsoGrid = () => {
               <li onClick={handleFilterKeyChange('residential')}>Residential</li>
               <li onClick={handleFilterKeyChange('office')}>Office</li>
               <li onClick={handleFilterKeyChange('adaptive-reuse')}>Adaptive Reuse</li>
+              <li onClick={handleFilterKeyChange('commercial')}>Commercial</li>
             </ul>
             <ul className="filter-container">
 
